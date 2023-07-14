d1bd990532b00b17386633148a77c980ac155a12
Gustavo Villegas <villegasgustavoopel@hotmail.com>
2023-07-14 12:30:58 -0300

    creando primera entrega Proyecto Final

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { HomeModule } from './pages/home/home.module';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+   
+  ],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    HomeModule,
+  ],
+  exports:[
+    DashboardComponent,
+  ]
+})
+export class DashboardModule { 
+  
+}
