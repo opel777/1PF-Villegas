106f5da21258eab5f3bc4c55fe7ac75aff0d204e
Gustavo Villegas <villegasgustavoopel@hotmail.com>
2023-07-18 01:03:10 -0300

    creando ususarios

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { UserFormDialogComponent } from './components/user-form-dialog/user-form-dialog.component';
-
+import { User } from './model';
+const ELEMENT_DATA: User[] = [
+  {
+    id:1,
+    name:'Marcos',
+    surname:'Perez',
+    email:'MarcorPerez@hotmail.com',
+    password:'Marcos1234'
+  }
+];
 
 @Component({
   selector: 'app-users',
@@ -9,6 +18,8 @@ import { UserFormDialogComponent } from './components/user-form-dialog/user-form
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent {
+
+  public users:User[]=ELEMENT_DATA;
 constructor(
   private matDialog: MatDialog
 ){}
@@ -18,6 +29,17 @@ onCreateUser():void{
  dialogRef.afterClosed().subscribe({
   next:(v) =>{
     if(v){
+
+      this.users=[
+        ...this.users,
+{ id:this.users.length + 1,
+  name: v.name,
+  surname:v.surname,
+  email:v.email,
+  password:v.password
+  },
+      ];
+        
       console.log('Recibimos el valor:',v);
     } else {
       console.log('Se cancelo');
